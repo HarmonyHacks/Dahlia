96fc5267170906acb51b9e298793f294794549dc
Adron Hall <adronhall@gmail.com>
2011-02-18 22:29:27 -0800

    Adding the first few views and some models just to work from.  Don't freak out :)

--- Dahlia/Dahlia/Controllers/HomeController.cs
+++ Dahlia/Dahlia/Controllers/HomeController.cs
@@ -1,17 +1,9 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Dahlia.Controllers
 {
     public class HomeController : Controller
     {
-        //
-        // GET: /Home/
-
         public ActionResult Index()
         {
             return View();
---
+++ Dahlia/Dahlia/Models/Retreat.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dahlia.Models
+{
+    public class Retreat
+    {
+    }
+}
