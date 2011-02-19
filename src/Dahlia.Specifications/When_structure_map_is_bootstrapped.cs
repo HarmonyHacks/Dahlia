3768f7417846e12675e691cc5b0813b1551b2b7f
Eric Lee <saintgimp@hotmail.com>
2011-02-19 12:07:08 -0800

    Added log4net to the projects

--- src/Dahlia.Specifications/When_structure_map_is_bootstrapped.cs
+++ src/Dahlia.Specifications/When_structure_map_is_bootstrapped.cs
@@ -1,9 +1,11 @@
 using Dahlia.Configuration;
+using log4net;
 using Machine.Specifications;
 using StructureMap;
 
 namespace Dahlia.Specifications
 {
+    [Subject("Configuration")]
     public class When_structure_map_is_bootstrapped
     {
         Because of = () =>
@@ -11,5 +13,24 @@ namespace Dahlia.Specifications
 
         It should_create_a_valid_container_configuration = () =>
             ObjectFactory.AssertConfigurationIsValid();
+
+        It should_create_a_valid_logger_instance = () =>
+            ObjectFactory.GetInstance<ILog>().ShouldNotBeNull();
+    }
+
+    public class When_resolving_a_class_with_a_logger_property
+    {
+        Establish context = AppStart_Structuremap.Start;
+        
+        Because of = () => { };
+
+        It should_populate_the_logger = () =>
+                ObjectFactory.GetInstance<SomeClass>().Logger.ShouldNotBeNull();
+    }
+
+
+    internal class SomeClass
+    {
+        public ILog Logger { get; set; }
     }
 }
