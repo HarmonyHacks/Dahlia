ec2a76e6564caa31cd8abaf18d298b5c3d8540fb
kelly.leahy <kelly.p.leahy@gmail.com>
2011-02-19 13:44:53 -0800

    add RetreatJs controller to get JS stuff started

---
+++ src/Dahlia/Controllers/RetreatJsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Dahlia.Repositories;
+using Dahlia.ViewModels;
+
+namespace Dahlia.Controllers
+{
+    public class RetreatJsController : Controller
+    {
+        readonly IRetreatRepository _retreatRepository;
+
+        public RetreatJsController(IRetreatRepository retreatRepository)
+        {
+            _retreatRepository = retreatRepository;
+        }
+
+        public JsonResult Index()
+        {
+            var model = new RetreatListViewModel
+            {
+                CreateLink = new Uri("/Retreat/Create", UriKind.Relative),
+
+                Retreats = _retreatRepository.GetList().Select(x => new RetreatListRetreatViewModel
+                {
+                    Date = x.StartDate,
+                    AddParticipantLink = new Uri("../Participant/AddToRetreat?retreatDate=" + x.StartDate.ToString("d"), UriKind.Relative)
+                })
+            };
+
+            return Json(model, "text/text", JsonRequestBehavior.AllowGet);
+        }
+    }
+}
