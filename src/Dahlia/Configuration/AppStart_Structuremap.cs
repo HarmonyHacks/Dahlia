b002e246a04ff856f49de4a666b2972af3ea5bde
Eric Lee <saintgimp@hotmail.com>
2011-02-19 14:32:19 -0800

    Switching back to the MCV3 dependency resolver

--- src/Dahlia/Configuration/AppStart_Structuremap.cs
+++ src/Dahlia/Configuration/AppStart_Structuremap.cs
@@ -8,8 +8,7 @@ namespace Dahlia.Configuration {
     public static class AppStart_Structuremap {
         public static void Start() {
             var container = IoC.Initialize();
-            //DependencyResolver.SetResolver(new SmDependencyResolver(container));
-            ControllerBuilder.Current.SetControllerFactory(new SmControllerFactory(container));
+            DependencyResolver.SetResolver(new SmDependencyResolver(container));
         }
     }
 }
--- src/Dahlia/Configuration/SmDependencyResolver.cs
+++ src/Dahlia/Configuration/SmDependencyResolver.cs
@@ -7,7 +7,6 @@ using StructureMap;
 
 namespace Dahlia.Configuration
 {
-    /*
     public class SmDependencyResolver : IDependencyResolver {
 
         private readonly IContainer _container;
@@ -22,32 +21,14 @@ namespace Dahlia.Configuration
             
             if (serviceType.IsAbstract || serviceType.IsInterface)
             {
-                var instance = _container.TryGetInstance(serviceType);
-                if (instance == null)
-                    throw new InvalidOperationException("Could not resolve component for service: " + serviceType.FullName);
-                return instance;
+                return _container.TryGetInstance(serviceType);
             }
+
             return _container.GetInstance(serviceType);
         }
 
         public IEnumerable<object> GetServices(Type serviceType) {
             return _container.GetAllInstances<object>().Where(s => s.GetType() == serviceType);
-        }
-    }
-    */
-
-    public class SmControllerFactory: DefaultControllerFactory
-    {
-        private readonly IContainer _container;
-
-        public SmControllerFactory(IContainer container)
-        {
-            _container = container;
-        }
-
-        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
-        {
-            return (IController)_container.GetInstance(controllerType);
         }
     }
 }
