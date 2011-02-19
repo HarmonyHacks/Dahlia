81aff8a219ba4b77c389ce4469f8c79612eeb343
kelly.leahy <kelly.p.leahy@gmail.com>
2011-02-19 10:52:07 -0800

    get data models in sync with iteration 1

--- src/Dahlia/Models/Room.cs
+++ src/Dahlia/Models/Room.cs
@@ -5,9 +5,11 @@ using System.Text;
 
 namespace Dahlia.Models
 {
+    /*
     public class Room
     {
         public virtual int Id { get; set; }
         public virtual string RoomCode { get; set; }
     }
+    */
 }
