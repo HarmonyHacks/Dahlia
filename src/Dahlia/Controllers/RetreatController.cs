545fd5871cb14e0cb117efff4139a775f30d95d7
Adron Hall <adronhall@gmail.com>
2011-02-19 10:25:03 -0800

    Adding the basic retreat create page.

--- src/Dahlia/Controllers/RetreatController.cs
+++ src/Dahlia/Controllers/RetreatController.cs
@@ -1,20 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace Dahlia.Controllers
 {
     public class RetreatController : Controller
     {
-        //
-        // GET: /Retreat/
-
-        public ActionResult Index()
+         public ActionResult Index()
         {
             return View();
         }
 
+        public ActionResult Create()
+        {
+            return View();
+        }
     }
 }
--- src/Dahlia/Models/Retreat.cs
+++ src/Dahlia/Models/Retreat.cs
@@ -5,6 +5,6 @@ namespace Dahlia.Models
     public class Retreat
     {
         public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
+        public string Description { get; set; }
     }
 }
