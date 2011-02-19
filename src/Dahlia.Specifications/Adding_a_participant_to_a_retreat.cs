c71eb3b302510c2acde6b1a5cbb506dca314c9c0
kelly.leahy <kelly.p.leahy@gmail.com>
2011-02-19 01:31:10 -0800

    progress on adding a participant to a retreat

--- src/Dahlia.Specifications/Adding_a_participant_to_a_retreat.cs
+++ src/Dahlia.Specifications/Adding_a_participant_to_a_retreat.cs
@@ -4,20 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Web.Mvc;
 using Dahlia.Controllers;
+using Dahlia.Models;
 using Dahlia.ViewModels;
 using Machine.Specifications;
+using Rhino.Mocks;
 
-namespace Dahlia.Specifications.Adding_a_participant_to_a_retreat
+namespace Dahlia.Specifications
 {
+    [Subject("Adding a participant to a retreat")]
     public class When_showing_the_add_participant_screen_for_a_retreat
     {
         Establish context = () =>
                             {
                                 _retreatDate = new DateTime(2007, 12, 15);
-                                _controller = new ParticipantController();
+                                _controller = new ParticipantController(null);
                             };
 
-        private Because of = () =>
+        Because of = () =>
                                  {
                                      _viewResult = _controller.AddToRetreat(_retreatDate);
                                      _viewModel = (AddParticipantToRetreatViewModel) _viewResult.ViewData.Model;
@@ -27,11 +30,41 @@ namespace Dahlia.Specifications.Adding_a_participant_to_a_retreat
 
         It should_return_a_view_model_that_contains_the_retreat_date = () => _viewModel.RetreatDate.ShouldEqual(_retreatDate);
 
-        It should_have_a_default_date_equal_to_today_for_the_received_date = () => _ViewModel.DateReceived.ShouldEqual(DateTime.Today);
+        It should_have_a_default_date_equal_to_today_for_the_received_date = () => _viewModel.DateReceived.ShouldEqual(DateTime.Today);
 
         static DateTime _retreatDate;
         static ViewResult _viewResult;
         static AddParticipantToRetreatViewModel _viewModel;
         static ParticipantController _controller;
+    }
+
+    [Subject("Adding a participant to a retreat")]
+    public class When_posting_back_from_the_add_participant_screen_for_a_retreat
+    {
+        Establish context = () =>
+        {
+            _viewModel = new AddParticipantToRetreatViewModel
+                         {
+                             RetreatDate = new DateTime(2007, 12, 15),
+                             BedCode = "bedcode",
+                             FirstName = "firstbob",
+                             LastName = "lastmartin",
+                             DateReceived = DateTime.Today,
+                             Notes = "yada yada yada...",
+                         };
+            _retreatDate = new DateTime(2007, 12, 15);
+            
+            _retreatParticipantAdder = MockRepository.GenerateStub<IRetreatParticipantAdder>();
+            _controller = new ParticipantController(_retreatParticipantAdder);
+        };
+
+        Because of = () => _controller.DoAddToRetreat(_viewModel);
+
+        It should_add_the_participant = () => _retreatParticipantAdder.AssertWasCalled(x => x.AddParticipantToRetreat(Arg<DateTime>.Is.Equal(_retreatDate), Arg<Participant>.Is.Anything));
+
+        static DateTime _retreatDate;
+        static AddParticipantToRetreatViewModel _viewModel;
+        static ParticipantController _controller;
+        static IRetreatParticipantAdder _retreatParticipantAdder;
     }
 }
--- src/Dahlia/Controllers/ParticipantController.cs
+++ src/Dahlia/Controllers/ParticipantController.cs
@@ -1,11 +1,24 @@
 ﻿using System;
 using System.Web.Mvc;
+using Dahlia.Models;
 using Dahlia.ViewModels;
 
 namespace Dahlia.Controllers
 {
+    public interface IRetreatParticipantAdder
+    {
+        void AddParticipantToRetreat(DateTime retreatDate, Participant participant);
+    }
+
     public class ParticipantController : Controller
     {
+        readonly IRetreatParticipantAdder _retreatParticipantAdder;
+
+        public ParticipantController(IRetreatParticipantAdder retreatParticipantAdder)
+        {
+            _retreatParticipantAdder = retreatParticipantAdder;
+        }
+
         public ViewResult AddToRetreat(DateTime retreatDate)
         {
             var viewModel = new AddParticipantToRetreatViewModel
@@ -16,5 +29,10 @@ namespace Dahlia.Controllers
             return View("AddToRetreat", viewModel);
         }
 
+        public ActionResult DoAddToRetreat(AddParticipantToRetreatViewModel postBack)
+        {
+            _retreatParticipantAdder.AddParticipantToRetreat(postBack.RetreatDate, null);
+            return new EmptyResult();
+        }
     }
 }
