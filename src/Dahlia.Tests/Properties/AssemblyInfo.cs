64bb2d680fa29a6d064dd56b772f5a9d852bfea6
unknown <BoJohnso@.prod.ds.russell.com>
2011-02-18 23:03:24 -0800

    created basic build that compiles the solution to the CI\Work directory

--- src/Dahlia.Tests/Properties/AssemblyInfo.cs
+++ src/Dahlia.Tests/Properties/AssemblyInfo.cs
@@ -8,29 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Dahlia.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Dahlia.Tests")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f3670420-a568-4f89-9a11-2c9a96c097a9")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
