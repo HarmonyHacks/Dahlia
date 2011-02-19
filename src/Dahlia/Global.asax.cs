af98d7136ad3d49ed1be49a54661a329d53d6ea6
kelly.leahy <kelly.p.leahy@gmail.com>
2011-02-19 11:49:52 -0800

    remove home controller, redirect main page to retreat list

--- src/Dahlia/Global.asax.cs
+++ src/Dahlia/Global.asax.cs
@@ -24,7 +24,7 @@ namespace Dahlia
             routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+                new { controller = "Retreat", action = "Index", id = UrlParameter.Optional } // Parameter defaults
             );
 
         }
