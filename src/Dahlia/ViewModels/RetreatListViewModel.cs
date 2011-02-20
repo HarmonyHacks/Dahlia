3accec0bea123b62383383c73d4d14d5ee58bdcf
TerryH <terry@tnthughes.net>
2011-02-19 17:00:43 -0800

    Added back the needed property

--- src/Dahlia/ViewModels/RetreatListViewModel.cs
+++ src/Dahlia/ViewModels/RetreatListViewModel.cs
@@ -13,6 +13,7 @@ namespace Dahlia.ViewModels
     {
         public DateTime Date { get; set; }
         public Uri AddParticipantLink { get; set; }
+        public IEnumerable<RetreatListParticipantViewModel> RegisteredParticipants { get; set; }
     }
 
     public class RetreatListParticipantViewModel
