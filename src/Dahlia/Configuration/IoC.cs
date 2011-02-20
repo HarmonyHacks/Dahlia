1c6024de4eb28d4c2236357b88f07a4007123cdb
Eric Lee <saintgimp@hotmail.com>
2011-02-20 02:06:17 -0800

    Changed the lifetime of the ISession to hybrid so that AssertContainerIsValid still works in tests

--- src/Dahlia/Configuration/IoC.cs
+++ src/Dahlia/Configuration/IoC.cs
@@ -17,7 +17,7 @@ namespace Dahlia.Configuration {
                                         scan.WithDefaultConventions();
                                     });
                             x.For<ISessionFactory>().Singleton().Use(SQLSessionFactory.CreateSessionFactory());
-                            x.For<ISession>().HttpContextScoped().Use(context => context.GetInstance<ISessionFactory>().OpenSession());
+                            x.For<ISession>().HybridHttpOrThreadLocalScoped().Use(context => context.GetInstance<ISessionFactory>().OpenSession());
                             x.For<ILog>().Use(LogManager.GetLogger("Dahlia"));
                             x.For<RouteCollection>().Use(RouteTable.Routes);
                             x.FillAllPropertiesOfType<ILog>().Use(LogManager.GetLogger("Dahlia"));
