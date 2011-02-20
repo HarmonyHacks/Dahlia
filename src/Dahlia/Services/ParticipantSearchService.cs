d36f082a410cc1e10ec4c8d97254325d71f2735b
kelly.leahy <kelly.p.leahy@gmail.com>
2011-02-20 12:09:35 -0800

    work in progress on searching participants for add screen

---
+++ src/Dahlia/Services/ParticipantSearchService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Dahlia.Models;
+
+namespace Dahlia.Services
+{
+    public interface IParticipantSearchService
+    {
+        IEnumerable<Participant> SearchParticipants(string firstName, string lastName);
+    }
+
+    public class ParticipantSearchService: IParticipantSearchService
+    {
+        public IEnumerable<Participant> SearchParticipants(string firstName, string lastName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
