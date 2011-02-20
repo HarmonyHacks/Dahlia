6a9a1bad745bcfe01e887fda75ddf7bb51b4cb77
Eric Lee <saintgimp@hotmail.com>
2011-02-19 18:36:20 -0800

    flushing out initial backbone work.. displaying retreats as non-interactive tabs..

--- src/Dahlia/Scripts/dahlia.js
+++ src/Dahlia/Scripts/dahlia.js
@@ -1,3 +1,4 @@
+var global = this;
 (function () {
     //
     var Retreat = Backbone.Model.extend({
@@ -6,7 +7,48 @@
     });
 
     var RetreatCollection = Backbone.Collection.extend({
-        
+        model: Retreat,
+        localStore: new Store("retreats")
+    });
+
+    var retreatsStore = new RetreatCollection();
+
+    var RetreatView = Backbone.View.extend({
+        retreatsTempl: null,
+        initialize: function () {
+            this.retreatsTempl = _.template($('#retreat_templ').html());
+        },
+
+        render: function() {
+            var m = this.model.toJSON();
+            $(this.el).html(this.retreatsTempl(m));
+            $(this.el).css('display', 'inline');
+            return this;
+        }
+    });
+
+    global.RetreatsUI = Backbone.View.extend({
+        initialize: function () {
+            retreatsStore.bind('add', this.addRetreat);
+            retreatsStore.bind('all', this.render);
+
+            var retreats = this.options.rawRetreats;
+
+            _.each(retreats, function (r) {
+                r.id = r.StartDateStr;
+                retreatsStore.add(r);
+            });
+        },
+
+        render: function () {
+            return this;
+        },
+
+        addRetreat: function (retreat) {
+            var view = new RetreatView({model: retreat});
+            $('#retreats_list').append(view.render().el);
+        }
+
     });
 
 })()
