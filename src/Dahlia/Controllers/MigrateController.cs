fb2642fa4be4af9a9a3728301ff6f730f38e98e3
Bobby Johnson <bobby.johnson@gmail.com>
2011-02-24 08:32:00 -0800

    built a simple migration interface that allows us to migrate a DB to specific versions

---
+++ src/Dahlia/Controllers/MigrateController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Dahlia.Services;
+
+namespace Dahlia.Controllers
+{
+    public class MigrateController : Controller
+    {
+        readonly IMigrationService _migrator;
+
+        public MigrateController(IMigrationService migrator)
+        {
+            _migrator = migrator;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Up(int? id)
+        {
+            _migrator.MigrateUp(id);
+
+            return new ContentResult{Content = "Migrated!"};
+        }
+
+        public ActionResult Down(int id)
+        {
+            _migrator.MigrateDown(id);
+
+            return new ContentResult { Content = "Downgraded!" };
+        }
+
+    }
+}
---
+++ src/Dahlia/Migrations/001_Add_Beds.cs
@@ -0,0 +1,54 @@
+using System;
+using Dahlia.Models;
+using FluentMigrator;
+
+namespace Dahlia.Migrations
+{
+    [Migration(001)]
+    public class Add_Beds : Migration
+    {
+        public override void Up()
+        {
+            Create.Table<Bed>()
+                .WithColumn<Bed>(x => x.Id).AsInt32().Identity()
+                .WithColumn<Bed>(x => x.Code).AsAnsiString(128).NotNullable()
+                .WithColumn<Bed>(x => x.IsUpstairs).AsBoolean().NotNullable();
+
+            Insert.IntoTable<Bed>()
+                .Row(new {Code = "GH1-1", IsUpstairs = false})
+                .Row(new {Code = "GH1-2", IsUpstairs = false})
+                .Row(new {Code = "GH2-1", IsUpstairs = false})
+                .Row(new {Code = "GH2-2", IsUpstairs = false})
+                .Row(new {Code = "GH3-1", IsUpstairs = false})
+                .Row(new {Code = "GH3-2", IsUpstairs = false})
+                .Row(new {Code = "CS2-1", IsUpstairs = false})
+                .Row(new {Code = "CS2-2", IsUpstairs = false})
+                .Row(new {Code = "CS3-1", IsUpstairs = false})
+                .Row(new {Code = "CS3-2", IsUpstairs = false})
+                .Row(new {Code = "CS4-1", IsUpstairs = false})
+                .Row(new {Code = "CS4-2", IsUpstairs = false})
+                .Row(new {Code = "CS5-1", IsUpstairs = false})
+                .Row(new {Code = "CS5-2", IsUpstairs = false})
+                .Row(new {Code = "CS6-1", IsUpstairs = true})
+                .Row(new {Code = "CS6-2", IsUpstairs = true})
+                .Row(new {Code = "CS7-1", IsUpstairs = true})
+                .Row(new {Code = "CS7-2", IsUpstairs = true})
+                .Row(new {Code = "L1-1'", IsUpstairs = false})
+                .Row(new {Code = "L2-1'", IsUpstairs = false})
+                .Row(new {Code = "L2-2'", IsUpstairs = false})
+                .Row(new {Code = "C1-1'", IsUpstairs = false})
+                .Row(new {Code = "C1-2'", IsUpstairs = false})
+                .Row(new {Code = "C2-1'", IsUpstairs = false})
+                .Row(new {Code = "C2-2'", IsUpstairs = false})
+                .Row(new {Code = "C3-1'", IsUpstairs = false})
+                .Row(new {Code = "C3-2'", IsUpstairs = false})
+                .Row(new {Code = "C4-1'", IsUpstairs = false})
+                .Row(new {Code = "C4-2'", IsUpstairs = false});
+        }
+
+        public override void Down()
+        {
+            Delete.Table<Bed>();
+        }
+    }
+}
