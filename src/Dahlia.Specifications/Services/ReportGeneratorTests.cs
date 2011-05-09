36cf1bb34cb875e1b56cc50681dd3ad81eef1aec
Jeff Olson <olson.jeffery@gmail.com>
2011-05-09 09:52:13 -0700

    specs/impl to close gh-18 . do a null check on Bed when building report .. also sorting participants in a retreat.. waitlisters at the bottom.

--- src/Dahlia.Specifications/Services/ReportGeneratorTests.cs
+++ src/Dahlia.Specifications/Services/ReportGeneratorTests.cs
@@ -3,27 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Machine.Specifications;
+using Dahlia.Services;
+using Rhino.Mocks;
+using Dahlia.Repositories;
+using Dahlia.Models;
 
 namespace Dahlia.Specifications.Services
 {
-    public class when_doing_something : ReportGeneratorTests
+    public class when_generating_a_reporting_for_a_Retreat_with_a_participant_who_is_waitlisted : ReportGeneratorTests
     {
         Establish context = () =>
         {
+            var retreat = new Retreat();
+            var participant = new Participant
+            {
+                DateReceived = DateTime.Now,
+                FirstName = "Johnny",
+                LastName = "Tentpeg",
+                PhysicalStatus = PhysicalStatus.Unlimited,
+                Notes = "something witty",
+                Id = 0
+            };
+            Bed bed = null; // waitlisted Participants have a null bedcode
+            retreat.AddParticipant(participant, bed);
+            retreatRepo.Expect(rr => rr.GetById(0)).IgnoreArguments().Return(retreat);
             
+            error = Catch.Exception(() =>
+                reportResult = reportGenerator.GenerateRetreatReportExcelHtmlFor(0)
+            );
         };
 
-        It should_do_something = () =>
-            foo.ShouldEqual("bar");
+        It should_not_cause_an_error = () =>
+            error.ShouldBeNull();
+
+        It should_return_the_generated_report = () =>
+            reportResult.ShouldNotBeNull();
     }
 
     public class ReportGeneratorTests
     {
         Establish context = () =>
         {
-            foo = "bar";
+            retreatRepo = MockRepository.GenerateStub<IRetreatRepository>();
+            reportGenerator = new ReportGeneratorService(retreatRepo);
         };
 
-        public static string foo;
+        Cleanup after = () =>
+        {
+            error = null;
+            reportResult = null;
+        };
+
+
+        public static IRetreatRepository retreatRepo;
+        public static IReportGeneratorService reportGenerator;
+        public static IEnumerable<byte> reportResult;
+        public static Exception error;
     }
 }
