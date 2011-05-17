a729b7b87e28fe18ddbe5b7687a07af1cb69c94e
Eric Lee <saintgimp@hotmail.com>
2011-05-17 00:14:30 -0700

    Changed the database code to use a disk-based SQLite database by default for development purposes, or it can use a SQL Server databse (Express or full) in production.  The type of database to use is inferred from the connection string, where anything referring to a .db file as the data source is considered to be a SQLite db and anything else is SQL Server.  This closes GH-17.

--- src/Dahlia/Configuration/Persistence/Migrations/SqlMigrationRunner.cs
+++ src/Dahlia/Configuration/Persistence/Migrations/SqlMigrationRunner.cs
@@ -2,30 +2,50 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
+using System.Data.SQLite;
+using System.Globalization;
+using System.Text.RegularExpressions;
 using Dahlia.Services;
+using FluentMigrator;
 using FluentMigrator.Runner;
 using FluentMigrator.Runner.Announcers;
 using FluentMigrator.Runner.Generators;
 using FluentMigrator.Runner.Initialization;
 using FluentMigrator.Runner.Processors;
+using FluentMigrator.Runner.Processors.Sqlite;
 using FluentMigrator.Runner.Processors.SqlServer;
 
 namespace Dahlia.Configuration.Persistence.Migrations
 {
     public class SqlMigrationService : IMigrationService
     {
-        
-       MigrationRunner getRunner()
+        MigrationRunner getRunner()
         {
             var connectionString = ConfigurationManager.ConnectionStrings["dahliaSQL"].ConnectionString;
-            var connection = new SqlConnection(connectionString);
+            
+            var processor = GetProcessor(connectionString);
+
             var assembly = typeof(SqlMigrationService).Assembly;
             var ns = typeof(SqlMigrationService).Namespace;
-            var generator = new SqlServer2005Generator();
-            var processor = new SqlServerProcessor(connection, generator, new NullAnnouncer(), new ProcessorOptions());
 
             return new MigrationRunner(assembly, new RunnerContext(new NullAnnouncer()) { Namespace = ns },
-                                          processor);
+                                            processor);
+        }
+
+        IMigrationProcessor GetProcessor(string connectionString)
+        {
+            if (SQLSessionFactory.IsSqliteConnectionString(connectionString))
+            {
+                var connection = new SQLiteConnection(connectionString);
+                var generator = new SqliteGenerator();
+                return new SqliteProcessor(connection, generator, new NullAnnouncer(), new ProcessorOptions());
+            }
+            else
+            {
+                var connection = new SqlConnection(connectionString);
+                var generator = new SqlServer2008Generator();
+                return new SqlServerProcessor(connection, generator, new NullAnnouncer(), new ProcessorOptions());
+            }
         }
 
         public void MigrateUp(long? version)
